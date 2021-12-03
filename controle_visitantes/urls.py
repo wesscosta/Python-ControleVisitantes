db428b3603b774ac631131e9982aae4fcecfe166
Weslley Costa <weslleycosta.ti@gmail.com>
2021-12-03 10:47:09 -0300

    Criação do Modelo Usuario

---
+++ controle_visitantes/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from usuarios.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path(
+        '',
+        index,
+        name='index',
+    )
+]
